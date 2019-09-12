ac7e43fcf5afd2e03564877d9597d7686a2ff0e4
accuradev <dev.marchenko@gmail.com>
2019-09-12 13:27:38 +0300

    fix(authentication): fix token preparation logic
    Make token cookies http only and fix expires time.

--- schema/User.ts
+++ schema/User.ts
@@ -1,4 +1,4 @@
-import { gql, IResolvers } from 'apollo-server-express';
+import { gql, IResolvers, AuthenticationError, UserInputError } from 'apollo-server-express';
 import * as bcrypt from 'bcrypt';
 
 import { User } from '../db/entities/User';
@@ -15,11 +15,11 @@ export const typeDefs = gql`
     user: User
   }
 
-  type Query {
+  extend type Query {
     getCurrentUser: User
   }
 
-  type Mutation {
+  extend type Mutation {
     login(email: String!, password: String!): AuthResponse
     signup(username: String!, email: String!, password: String!): AuthResponse
     invalidateTokens: Boolean!
@@ -30,7 +30,7 @@ export const resolvers: IResolvers = {
   Query: {
     getCurrentUser: (_, __, { req }) => {
       if (!req.userId) {
-        return null;
+        throw new AuthenticationError('You must be logged in');
       }
 
       return User.findOne(req.userId);
@@ -51,12 +51,12 @@ export const resolvers: IResolvers = {
       res.cookie('refresh-token', refreshToken, {
         expires: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
         secure: true,
-        httpOnly: false,
+        httpOnly: true,
       });
       res.cookie('access-token', accessToken, {
-        expires: new Date(new Date().getTime() + 10 * 60 * 1000),
+        expires: new Date(new Date().getTime() + 15 * 60 * 1000),
         secure: true,
-        httpOnly: false,
+        httpOnly: true,
       });
 
       return {
@@ -66,22 +66,26 @@ export const resolvers: IResolvers = {
     },
     login: async (_, { email, password }, { res }) => {
       const user = await User.findOne({ where: { email } });
-      if (!user) return null;
+      if (!user) {
+        throw new UserInputError('Invalid Email or Password');
+      }
 
       const valid = await bcrypt.compare(password, user.password);
-      if (!valid) return null;
+      if (!valid) {
+        throw new UserInputError('Invalid Email or Password');
+      }
 
       const { accessToken, refreshToken } = createTokens(user);
 
       res.cookie('refresh-token', refreshToken, {
         expires: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
         secure: true,
-        httpOnly: false,
+        httpOnly: true,
       });
       res.cookie('access-token', accessToken, {
-        expires: new Date(new Date().getTime() + 10 * 60 * 1000),
+        expires: new Date(new Date().getTime() + 15 * 60 * 1000),
         secure: true,
-        httpOnly: false,
+        httpOnly: true,
       });
 
       return {
