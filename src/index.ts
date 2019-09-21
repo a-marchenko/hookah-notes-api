deb19388f4fc73c413266faa43cb59517e49ed94
accuradev <dev.marchenko@gmail.com>
2019-09-21 15:50:17 +0300

    feat(auth): provide cookies and CORS management, token refreshment logic and route

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ import { createConnection } from 'typeorm';
 
 // Server
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+// JWT
+import { verify } from 'jsonwebtoken';
 
 // GraphQL
 import { ApolloServer } from 'apollo-server-express';
@@ -15,13 +20,17 @@ import { buildSchema } from 'type-graphql';
 import chalk from 'chalk';
 
 // Local
+import { refreshSecret, createAccessToken, sendRefreshToken, createRefreshToken } from './services/auth';
 import { GraphqlServerContext } from './interfaces/GraphqlServerContext';
 import { UserResolver } from './resolvers/UserResolver';
+import { User } from './entity/User';
+import { AuthPayload } from './interfaces/AuthPayload';
 
 // ----------------------------------------------------------------------------
 
 // Config
 
+const CLIENT_URI = process.env.CLIENT_URI || 'https://localhost:8443';
 const PORT = process.env.PORT || '8000';
 
 /* App starts here */
@@ -30,6 +39,45 @@ const startServer = async () => {
   await createConnection();
 
   const app = express();
+
+  app.use(
+    cors({
+      origin: CLIENT_URI,
+      credentials: true,
+    }),
+  );
+
+  app.use(cookieParser());
+
+  app.post('/refresh_token', async (req, res) => {
+    const token = req.cookies.URT;
+
+    if (!token) {
+      return res.send({ ok: false, accessToken: '' });
+    }
+
+    let payload: AuthPayload;
+    try {
+      payload = verify(token, refreshSecret) as AuthPayload;
+    } catch (e) {
+      console.log(chalk.redBright(e));
+      return res.send({ ok: false, accessToken: '' });
+    }
+
+    const user = await User.findOne(payload.id);
+
+    if (!user) {
+      return res.send({ ok: false, accessToken: '' });
+    }
+
+    if (!(user.tokenVersion !== payload.tokenVersion)) {
+      return res.send({ ok: false, accessToken: '' });
+    }
+
+    sendRefreshToken(res, createRefreshToken(user));
+
+    return res.send({ ok: true, accessToken: createAccessToken(user) });
+  });
 
   const apollo = new ApolloServer({
     schema: await buildSchema({
@@ -40,7 +88,7 @@ const startServer = async () => {
     playground: true,
   });
 
-  apollo.applyMiddleware({ app });
+  apollo.applyMiddleware({ app, cors: false });
 
   app.listen(PORT, () => {
     console.log(
