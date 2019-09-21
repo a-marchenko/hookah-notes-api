657b325418aee72405b31680f8820f6f04913558
accuradev <dev.marchenko@gmail.com>
2019-09-21 03:30:10 +0300

    fix(auth): make using jwt secret keys more secure

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,14 +1,18 @@
 import { User } from '../entity/User';
 import { sign } from 'jsonwebtoken';
 
+export const accessSecret = process.env.JWT_ACCESS_SECRET || 'TMP_ACCESS_SECRET';
+
+export const refreshSecret = process.env.JWT_REFRESH_SECRET || 'TMP_REFRESH_SECRET';
+
 export const createAccessToken = (user: User) => {
-  return sign({ userId: user.id, username: user.username }, process.env.JWT_ACCESS_SECRET!, {
+  return sign({ userId: user.id, username: user.username }, accessSecret, {
     expiresIn: '15m',
   });
 };
 
 export const createRefreshToken = (user: User) => {
-  return sign({ userId: user.id, username: user.username }, process.env.JWT_ACCESS_SECRET!, {
+  return sign({ userId: user.id, username: user.username }, refreshSecret, {
     expiresIn: '7d',
   });
 };
