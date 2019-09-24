b42dcfdb834db69556b32f7006036deda454917f
accuradev <dev.marchenko@gmail.com>
2019-09-24 15:57:13 +0300

    feat(entities): add the Like entity

---
+++ src/entity/Like.ts
@@ -0,0 +1,20 @@
+import { ObjectType, Field, ID } from 'type-graphql';
+import { Entity, PrimaryGeneratedColumn, BaseEntity, ManyToOne } from 'typeorm';
+import { User } from './User';
+import { Note } from './Note';
+
+@ObjectType()
+@Entity('likes')
+export class Like extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field(() => User)
+  @ManyToOne(() => User, user => user.likes)
+  user: User;
+
+  @Field(() => Note)
+  @ManyToOne(() => Note, note => note.likes)
+  note: Note;
+}
