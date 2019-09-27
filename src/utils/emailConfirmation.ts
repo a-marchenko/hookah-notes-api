28a51aab1b440c7bcd2b0bce197e7d54dbe3d8c3
accuradev <dev.marchenko@gmail.com>
2019-09-27 18:00:11 +0300

    feat(auth): add finished confirmation email markup

--- src/utils/emailConfirmation.ts
+++ src/utils/emailConfirmation.ts
@@ -27,8 +27,8 @@ export const sendConfirmationEmail = async (email: string, username: string, url
   const info = await transporter.sendMail({
     from: '"HookahNotes" <hookahnotes@gmail.com>', // sender address
     to: email, // list of receivers
-    subject: 'Hookah Notes account confirmation', // Subject line
-    html: `<h3>Hello ${username}!</h3><p>Please, confirm your email by clicking the link below:</p><br><a href="${url}">${url}</a>`, // html body
+    subject: 'Hookah Notes email confirmation', // Subject line
+    html: `<div style="margin:10px auto;max-width:550px;font-size:14px;text-align:center;font-family:Helvetica,sans-serif;"><a href="#"><img src="https://i.ibb.co/2W40VKP/favicon.png" width="124px" height="124px"></a><div style="text-align:center;font-family:Helvetica,sans-serif;font-size:22px;font-weight:bold;margin:30px 0;color:#000000;">Hello from Hookah Notes!</div><p style="margin:40px 0">You have created a Hookah Notes account with username: <b>${username}</b>.<br>Please, confirm your email by clicking the link below:</p><a href="${url}" style="font-size:16px;background:#5f4591;padding:10px 70px;width:300px;color:#ffffff;border-radius:20px;text-decoration:none;">Confirm Your Email</a><div style="color:#70787d;font-size:12px;margin-top:20px;">This link will expire in 24 hours.</div></div>`, // html body
   });
 
   console.log('Message sent: %s', info.messageId);
