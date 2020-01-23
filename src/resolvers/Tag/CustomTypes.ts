84807d9912f96ef21a41d8191f4b87383ecd2239
accuradev <dev.marchenko@gmail.com>
2020-01-23 18:18:10 +0300

    feat(resolvers): add TagInput custom type

--- src/resolvers/Tag/CustomTypes.ts
+++ src/resolvers/Tag/CustomTypes.ts
@@ -1,9 +1,9 @@
 import { MinLength, MaxLength, Min, Max } from 'class-validator';
-import { InputType, Field, ID, Int } from 'type-graphql';
+import { InputType, Field, Int } from 'type-graphql';
 import { Tag } from '../../entity/Tag';
 
 @InputType({ description: 'New tag data' })
-export class AddTagInput implements Partial<Tag> {
+export class TagInput implements Partial<Tag> {
   @Field(() => String, { description: 'Title length can be from 2 to 16 characters' })
   @MaxLength(16)
   @MinLength(2)
@@ -13,10 +13,4 @@ export class AddTagInput implements Partial<Tag> {
   @Min(0)
   @Field(() => Int, { description: 'Color hue can be from 0 to 360' })
   hue: number;
-}
-
-@InputType({ description: 'Update tag data' })
-export class UpdateTagInput extends AddTagInput {
-  @Field(() => ID)
-  id: number;
 }
